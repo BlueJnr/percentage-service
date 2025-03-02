54b5aa6219473639ea67007d01d26281284ecab8
BlueJnr <kent.cuadros@unmsm.edu.pe>
2025-03-02 11:24:07 -0500

    feat: add retry mechanism for external service failures

--- src/main/java/com/tenpo/percentage/service/impl/PercentageServiceImpl.java
+++ src/main/java/com/tenpo/percentage/service/impl/PercentageServiceImpl.java
@@ -4,11 +4,13 @@ import com.tenpo.percentage.client.api.DefaultApi;
 import com.tenpo.percentage.config.exception.CacheValueNotFoundException;
 import com.tenpo.percentage.service.CacheService;
 import com.tenpo.percentage.service.PercentageService;
+import io.github.resilience4j.retry.annotation.Retry;
 import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PercentageServiceImpl implements PercentageService {
@@ -17,18 +19,20 @@ public class PercentageServiceImpl implements PercentageService {
   private final CacheService cacheService;
 
   @Override
+  @Retry(name = "percentageServiceRetry", fallbackMethod = "getCachedPercentage")
   public BigDecimal getValue() {
-    try {
-      BigDecimal value = defaultApi.externalPercentageGet().getValue();
-      cacheService.savePercentage(value);
-      return value;
-    } catch (RestClientException exception) {
-      return cacheService
-          .getPercentage()
-          .orElseThrow(
-              () ->
-                  new CacheValueNotFoundException(
-                      "No percentage available in cache and external service failed"));
-    }
+    log.info("Calling external service...");
+    BigDecimal value = defaultApi.externalPercentageGet().getValue();
+    cacheService.savePercentage(value);
+    return value;
+  }
+
+  public BigDecimal getCachedPercentage(Exception e) {
+    return cacheService
+        .getPercentage()
+        .orElseThrow(
+            () ->
+                new CacheValueNotFoundException(
+                    "No percentage available in cache and external service failed"));
   }
 }
